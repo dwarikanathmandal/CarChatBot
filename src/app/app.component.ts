ce59581510184753f3a4f9d1217a16714ab0e471
Dwarika Mandal <dwarika_mandal@persistent.co.in>
2017-05-29 10:03:39 +0530

    test travis ci

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<div class="container clearfix"><h1>Welcome to {{name}}</h1></div>
+  template: `<div class="container clearfix"><h1>Welcome to test change {{name}}</h1></div>
   <chat-box></chat-box>`,
 })
-export class AppComponent  { name = 'Virtuel Car Dashboard Assistant'; }
+export class AppComponent  
+{ 
+  name = 'Virtuel Car Dashboard Assistant';
+  address:string;
+
+  constructor(){
+    this.address ="Nagpur"
+  } 
+}
